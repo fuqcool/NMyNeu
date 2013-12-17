438fc13430d260c7d4dbf3f1bf9875abe35b23e8
fuqcool <fuqcool@gmail.com>
2013-12-16 21:48:23 -0500

    precise selector

--- open.js
+++ open.js
@@ -1,6 +1,6 @@
 (function ($) {
   var url = 'http://myneu.neu.edu/cp/ip/login?sys=sctssb&url=https://wl11gp.neu.edu/udcprod8/twbkwbis.P_GenMenu?name=bmenu.P_RegMnu';
-  var $reg = $('#p_chan_text a');
+  var $reg = $('a:contains("Course Registration")');
 
   $reg.click(function (evt) {
     evt.preventDefault();
